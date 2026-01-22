19a6aa94e49e30a4e774f386df7ad15660c5c7d5
unknown <khm@tuta.com>
2026-01-22 01:33:21 +0100

    auth+ a few things

--- projekt_backend2/db/initdb.sql
+++ projekt_backend2/db/initdb.sql
@@ -10,7 +10,7 @@ create table Admins (
 
 create table Products (
     id int auto_increment,
-    name varchar(64) not null,
+    name varchar(64) not null unique,
     category varchar(64),
     manufacturer varchar(64),
     description varchar(128),
@@ -59,11 +59,71 @@ create table Products_Orders (
 
 delimiter $$
 
-create procedure termekek(
-    p_from int, 
-    p_to int
+create function encrypt(p_password varchar(255))
+returns varchar(255) deterministic
+begin
+    return sha2(concat(p_password, 'cxvyz'), 256);
+end$$
+
+create trigger create_Visitors
+before insert on Visitors
+for each row
+begin
+    set new.password = encrypt(new.password);
+end$$
+
+create trigger update_Visitors
+before update on Visitors
+for each row
+begin
+    if new.password is not null and old.password <> new.password then
+        set new.password = encrypt(new.password);
+    end if;
+end$$
+
+create function login(
+    p_email varchar(64),
+    p_password varchar(255)
+)
+returns int
+reads sql data
+begin
+    declare uid int default 0;
+    select id
+        into uid
+    from Visitors
+    where
+        email = p_email and
+        password = encrypt(p_password);
+    return uid;
+end$$
+
+create procedure registration(
+    in p_name varchar(64),
+    in p_email varchar(64),
+    in p_password varchar(255)
 )
 begin
+    insert into Visitors (name, email, password) values
+        (p_name, p_email, p_password);
+end$$
+
+create procedure termekek(
+    in p_from int, 
+    in p_to int,
+    in p_name varchar(64),
+    in p_category varchar(64),
+    in p_manufacturer varchar(64),
+    in p_from_price int,
+    in p_to_price int
+)
+begin
+    declare v_limit int;
+    declare v_offset int;
+
+    set v_limit = p_to - p_from + 1;
+    set v_offset = p_from;
+
     select
         name as név,
         category as kategória,
@@ -73,20 +133,51 @@ begin
         price as ár,
         quantity as mennyiség
     from Products
-    limit p_from - p_to offset p_from;
+    where 
+        (p_name is null or name = p_name) and
+        (p_category is null or category = p_category) and
+        (p_manufacturer is null or manufacturer = p_manufacturer) and
+        (p_from_price is null or p_from_price = 0 or price >= p_from_price) and
+        (p_to_price is null or p_to_price = 0 or price <= p_to_price)
+    limit v_limit offset v_offset;
 end$$
 
-create procedure products(
-    p_which int,
-    p_name varchar(64),
-    p_category varchar(64),
-    p_manufacturer varchar(64),
-    p_from_price int,
-    p_to_price int,
+create procedure update_user(
+    in p_id int,
+    in p_name varchar(100),
+    in p_shipping_address varchar(255),
+    in p_billing_address varchar(255),
+    in p_email varchar(64),
+    in p_password varchar(255)
 )
 begin
-    select 
+    update Visitors set
+        name = coalesce(p_name, name),
+        shipping_address = coalesce(p_shipping_address, shipping_address),
+        billing_address = coalesce(p_billing_address, billing_address),
+        email = coalesce(p_email, email),
+        password = coalesce(p_password, password)
+    where id = p_id;
 end$$
-    
+
+create procedure get_user( 
+    in p_id int 
+)
+begin
+    select  
+        name as username,
+        email,
+        shipping_address,
+        billing_address
+    from Visitors
+    where id = p_id;
+end$$
+
+create procedure create_order(
+
+)
+begin
+
+end$$
 
 delimiter ;
--- projekt_backend2/webserver/main.js
+++ projekt_backend2/webserver/main.js
@@ -1,41 +1,151 @@
 const exp = require("express")
 const sql = require("mysql2/promise")
 const jwt = require("jsonwebtoken")
+const e = require("express")
 
+const SECRET = "hebelebobelepup"
 const PORT = 30000
+const DBConnP = {
+    host: "127.0.0.1", 
+    database: "main", 
+    user: "root", 
+    password: "1234"
+}
 
 const app = exp()
 app.use(exp.json())
 
-/**
- * @type {import("mysql2/promise").Connection}
- */
-let conn;
-
 app.get("/api/products", products)
+app.put("/api/order", auth, order)
+app.post("/api/login", login)
+app.put("/api/registration", registration)
+app.get("/api/user", auth, get_user)
+app.patch("/api/user", auth, update_user)
+ 
 
-_ = async function() {
-    try {
-        conn = await sql.createConnection({
-            host: "127.0.0.1", 
-            database: "main", 
-            user: "root", 
-            password: "1234"
-        })
-        console.log("Database connected!")
-    } catch (err) {
-        console.log(err)
-        console.log("FATAL -- Server can't connect to database!")
-    }
-    (conn) ? app.listen(PORT, () => { console.log(`Webserver started! Listening on port ${PORT}`) }) : false
-} ()
+app.listen(PORT, () => { console.log(`Webserver started! Listening on port ${PORT}`) })
 
 /**
  * @type {import("express").Handler}
  */
 async function products(req, res) {
-    const from = req.query.from
-    const to = req.query.to
+    const {from, to, name, category, manufacturer, from_price, to_price} = req.query
 
-    console.log(from, to)
+    if (!from || !to) {
+        return res.status(400).send("Missing parameter")
+    }
+
+    const conn = await sql.createConnection(DBConnP)
+    const [rows] = await conn.query("call termekek(?,?,?,?,?,?,?)", 
+        [from, to, name, category, manufacturer, from_price, to_price]
+    )
+    conn.end()
+
+    res.json(rows[0])
 }
+
+/** to be continnued
+ * @type {import("express").Handler}
+ */
+async function order(req, res) {
+    const products = req.body["products"]
+    const delivery = req.body["szállítási mód"]
+    const payment = req.body["fizetési mód"]
+
+    const conn = await sql.createConnection(DBConnP)
+
+
+    for (const p in products) {
+        const name = p["product name"]
+        const number = p["product number"]
+
+    }
+
+
+}
+
+/** 
+ * @type {import("express").Handler}
+ */
+async function login(req, res) {
+    const {email, password} = req.body
+
+    const conn = await sql.createConnection(DBConnP)
+    const [row] = await conn.query("select login(?,?) as id",
+        [email, password]
+    )
+    conn.end()
+
+    if (row[0].id == 0) {
+        return res.status(401).send("Bad email or password")
+    }
+
+    const token = jwt.sign({id: row[0].id}, SECRET)
+    res.json({token})
+}
+
+/**
+ * @type {import("express").Handler}
+ */
+async function auth(req, res, next) {
+    const token = req.headers["x-access-token"]
+
+    try {
+        const payload = jwt.verify(token, SECRET)
+        req.uid = payload.id
+    } catch {
+        return res.status(401).send("Unauthorized")
+    }
+    next()
+}
+
+/**
+ * @type {import("express").Handler}
+ */
+async function registration(req, res) {
+    const {email, name, password} = req.body
+    if (!email || !name || !password) { return res.status(400).send("bad request") }
+
+    const conn = await sql.createConnection(DBConnP)
+    try {
+        await conn.query("call registration(?,?,?)",
+            [name, email, password]
+        )
+    } catch (err) {
+        return res.status(400).send("Email is already in use")
+    }
+    conn.end()
+
+    res.status(201).send("User created")
+}
+
+/**
+ * @type {import("express").Handler}
+ */
+async function get_user(req, res) {
+
+    const conn = await sql.createConnection(DBConnP)
+    const [row] = await conn.query("call get_user(?)", 
+        [req.uid]
+    )
+    res.json(row[0])
+}
+
+/** It tryes to update email so it wont work
+ * @type {import("express").Handler}
+ */
+async function update_user(req, res) {
+    const {username, email, delivery_address, billing_address, password} = req.body
+
+    const conn = await sql.createConnection(DBConnP)
+    try {
+        await conn.query("call update_user(?,?,?,?,?,?)"
+            [req.uid, username, delivery_address, billing_address, email, password]
+        )
+    } catch {
+        return res.status(409).send("Email is already in use.")
+    }
+    res.status(202).send("Resource changed")
+}
+
+
